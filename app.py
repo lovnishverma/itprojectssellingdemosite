4bcbd62a4f37385631604121d2df3fbcde494ab6
Glitch (nielit-chandigarh) <none>
2023-08-03 14:42:18 +0000

    🎟🍳 Checkpoint ./app.py:57629255/15589 ./templates/admin_project_requests.html:57629255/1539 ./requirements.txt:57629255/2

--- app.py
+++ app.py
@@ -27,6 +27,21 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return "<User {}>".format(self.username)
 
+# Project model for the database table
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(100), nullable=False)
+    project_details = db.Column(db.Text, nullable=False)
+
+# ProjectRequest model for the database table
+class ProjectRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    user = db.relationship('User', backref='project_requests')
+    project = db.relationship('Project', backref='project_requests')
+    message_to_admin = db.Column(db.Text)  # New field for user message to admin
+
 # Load user function required by Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
@@ -35,13 +50,6 @@ def load_user(user_id):
 # Create the database tables
 db.create_all()
 
-# Project model for the database table
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(100), nullable=False)
-    project_details = db.Column(db.Text, nullable=False)
-    message_to_admin = db.Column(db.Text)  # New field for user message to admin
-
 # Form to create a new project
 class NewProjectForm(FlaskForm):
     project_name = StringField('Project Name', validators=[DataRequired()])
@@ -49,257 +57,22 @@ class NewProjectForm(FlaskForm):
     message_to_admin = TextAreaField('Message to Admin')  # Add the new message field
     submit = SubmitField('Create Project')
 
-@app.route('/')
+# ...
+
+@app.route('/admin/project_requests')
 @login_required
-def dashboard():
-    # Helper function to read and update visitor count
-    def get_visitor_count():
-        with open("visitor_count.txt", "r") as f:
-            count = int(f.read())
-        count += 1
-        with open("visitor_count.txt", "w") as f:
-            f.write(str(count))
-        return count
-    
-    # Get the current UTC time
-    utc_now = datetime.utcnow()
-
-    # Define the timezone for India (IST)
-    tz = pytz.timezone('Asia/Kolkata')
-
-    # Get the UTC offset for the India time zone
-    india_offset = timedelta(seconds=tz.utcoffset(utc_now).total_seconds())
-
-    # Add the UTC offset to the current UTC time to get India time
-    india_time = utc_now + india_offset
-
-    # Extract date and time components in 12-hour format
-    date = india_time.strftime("%Y-%m-%d")
-    time = india_time.strftime("%I:%M %p")
-
-    # Extract only the year from the date
-    year = india_time.strftime("%Y")
-
-    # Get the current hour in the India time zone
-    india_hour = india_time.hour
-
-    # Determine the time of day based on the current hour
-    if 5 <= india_hour < 12:
-        time_of_day = 'Morning'
-    elif 12 <= india_hour < 17:
-        time_of_day = 'Afternoon'
-    elif 17 <= india_hour < 21:
-        time_of_day = 'Evening'
-    else:
-        time_of_day = 'Night'
-    
-    # Get the visitor count
-    visitor_count = get_visitor_count()
-    username = current_user.username  # Get the username of the current user
-    
-    # Fetch all topics from the database
-    topics = Topic.query.all()
-
-    return render_template("main.html", topics=topics, username=username, time_of_day=time_of_day, date=date, time=time, year=year, visitor_count=visitor_count)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            flash('Logged in successfully.', 'success')
-            return redirect(url_for('dashboard'))
-
-        flash('Invalid username or password.', 'error')
-
-    return render_template('login.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        user = User.query.filter_by(username=username).first()
-
-        if user:
-            flash('Username already exists. Please choose a different username.', 'error')
-        else:
-            existing_user = User.query.filter_by(email=email).first()
-            if existing_user:
-                flash('Email already exists. Please choose a different email.', 'error')
-            else:
-                hashed_password = generate_password_hash(password)
-                new_user = User(username=username, email=email, password=hashed_password)
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Registration successful. You can now log in.', 'success')
-                return redirect(url_for('login'))
-
-    return render_template('register.html')
-
-@app.route('/request_project', methods=['GET', 'POST'])
-@login_required
-def request_project():
-    form = NewProjectForm()
-
-    if form.validate_on_submit():
-        project_name = form.project_name.data
-        project_details = form.project_details.data
-        message_to_admin = form.message_to_admin.data  # Get the user's message
-
-        # Store the project data and message in the database
-        new_project = Project(
-            project_name=project_name,
-            project_details=project_details,
-            message_to_admin=message_to_admin  # Save the user's message
-        )
-        db.session.add(new_project)
-        db.session.commit()
-
-        flash("Project request sent successfully.", 'success')
+def admin_project_requests():
+    # Make sure only the admin can access this view
+    if current_user.username != "admin":
+        flash("You do not have permission to access this page.", 'error')
         return redirect(url_for('dashboard'))
 
-    return render_template('request_project.html', form=form)
-  
-@app.route('/users')
-@login_required
-def list_users():
-    if current_user.username == "admin":
-        users = User.query.all()
-        return render_template('user_list.html', users=users)
-    else:
-        flash("You do not have permission to access Admin page.", 'error')
-        return redirect(url_for('dashboard'))
+    # Get all project requests from the database
+    project_requests = ProjectRequest.query.all()
 
-@app.route('/delete_user/<int:user_id>', methods=['POST'])
-@login_required
-def delete_user(user_id):
-    if current_user.username == "admin":
-        if current_user.id == user_id:
-            flash("You cannot delete your own account.", 'error')
-        else:
-            user = User.query.get(user_id)
-            if user:
-                db.session.delete(user)
-                db.session.commit()
-                flash("User '{}' has been deleted.".format(user.username), 'success')
-            else:
-                flash("User not found.", 'error')
-    else:
-        flash("You do not have permission to perform this action.", 'error')
-    return redirect(url_for('list_users'))
+    return render_template('admin_project_requests.html', project_requests=project_requests)
 
-
-# Topic model for the database table
-class Topic(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    topic_image = db.Column(db.String(200), nullable=False)
-    topic_name = db.Column(db.String(100), nullable=False)
-    topic_details = db.Column(db.Text, nullable=False)
-  
-
-# Admin Panel - Main Page
-@app.route('/admin')
-@login_required
-def admin_panel():
-    if current_user.is_authenticated and current_user.username == "admin":
-        return render_template('admin_panel.html')
-    
-    flash("You do not have permission to access the Admin panel.", 'error')
-    return redirect(url_for('dashboard'))
-
-# Admin Panel - Add New Topic
-@app.route('/admin/add_topic', methods=['GET', 'POST'])
-@login_required
-def add_topic():
-    if current_user.is_authenticated and current_user.username == "admin":
-        if request.method == 'POST':
-            topic_image = request.form['topicImage']
-            topic_name = request.form['topicName']
-            topic_details = request.form['topicDetails']
-            
-
-            # Store the topic data in the database
-            new_topic = Topic(
-                topic_image=topic_image,
-                topic_name=topic_name,
-                topic_details=topic_details,
-                
-            )
-            db.session.add(new_topic)
-            db.session.commit()
-            flash("Topic added successfully.", 'success')
-            return redirect(url_for('admin_panel'))
-
-        return render_template('add_topic.html')
-
-    flash("You do not have permission to access the Admin panel.", 'error')
-    return redirect(url_for('dashboard'))
-# Admin Panel - List Topics
-@app.route('/admin/list_topics')
-@login_required
-def list_topics():
-    if current_user.is_authenticated and current_user.username == "admin":
-        topics = Topic.query.all()
-        return render_template('list_topics.html', topics=topics)
-
-    flash("You do not have permission to access the Admin panel.", 'error')
-    return redirect(url_for('dashboard'))
-
-# Admin Panel - Modify Topic
-@app.route('/admin/modify_topic/<int:topic_id>', methods=['GET', 'POST'])
-@login_required
-def modify_topic(topic_id):
-    if current_user.is_authenticated and current_user.username == "admin":
-        topic = Topic.query.get(topic_id)
-
-        if topic is None:
-            flash("Topic not found.", 'error')
-            return redirect(url_for('admin_panel'))
-
-        if request.method == 'POST':
-            topic.topic_image = request.form['topicImage']
-            topic.topic_name = request.form['topicName']
-            topic.topic_details = request.form['topicDetails']
-           
-
-            db.session.commit()
-            flash("Topic updated successfully.", 'success')
-            return redirect(url_for('admin_panel'))
-
-        return render_template('modify_topic.html', topic=topic)
-
-    flash("You do not have permission to access the Admin panel.", 'error')
-    return redirect(url_for('dashboard'))
-
-# Admin Panel - Delete Topic
-@app.route('/admin/delete_topic/<int:topic_id>', methods=['POST'])
-@login_required
-def delete_topic(topic_id):
-    if current_user.is_authenticated and current_user.username == "admin":
-        topic = Topic.query.get(topic_id)
-
-        if topic is None:
-            flash("Topic not found.", 'error')
-        else:
-            db.session.delete(topic)
-            db.session.commit()
-            flash("Topic deleted successfully.", 'success')
-
-    return redirect(url_for('admin_panel'))
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logged out successfully.', 'success')
-    return redirect(url_for('login'))
+# ...
 
 if __name__ == '__main__':
     app.run(debug=True)
